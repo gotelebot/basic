b3db62f6a266ae411cf52d954923ffe62bf4e97e
Zero <54469706+3JoB@users.noreply.github.com>
2023-02-04 18:01:22 +0800

    upload

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/gotelebot/basic
+
+go 1.19
+
+require (
+	github.com/3JoB/telebot v0.0.0-20230204090104-6e432d9cfc3c
+	github.com/3JoB/ulib v0.0.6
+)
+
+require (
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/goccy/go-reflect v1.2.0 // indirect
+	github.com/grafana/regexp v0.0.0-20221122212121-6b5c0a4cb7fd // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+)
---
+++ handler.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	tele "github.com/3JoB/telebot"
+	tb "github.com/3JoB/ulib/telebot"
+)
+
+var (
+	btn = &tele.ReplyMarkup{}
+
+	alertbtn = btn.Data("Show Alert", "tele_show")
+	alert2btn = btn.Data("Show Next Alert", "tele_show2")
+)
+
+func showBtn() *tele.ReplyMarkup {
+	btn.Row(alertbtn, alert2btn)
+	return btn
+}
+
+func Handler(b *tele.Bot) {
+	b.Handle("/start", Start)
+	b.Handle("/autodelete", AutoStart)
+	b.Handle(&alertbtn, Alert1)
+	b.Handle(&alert2btn, Alert2)
+}
+
+func Start(c tele.Context) error {
+	c.Send("Hello World!")
+	return nil
+}
+
+func AutoStart(c tele.Context) error {
+	use := tb.New().SetContext(c)
+	//Messages will be automatically deleted after ten seconds.
+	use.SetAutoDelete(10).Send("Hello! Telebot!")
+	return nil
+}
+
+func SendBtn(c tele.Context) error {
+	use := tb.New().SetContext(c)
+	use.SetBtn(showBtn()).Send("choose a button")
+	return nil
+}
+
+func Alert1(c tele.Context) error {
+	use := tb.New().SetContext(c)
+	use.Alert("Wow! Alert1!")
+	return nil
+}
+
+func Alert2(c tele.Context) error {
+	use := tb.New().SetContext(c)
+	use.SetShowAlert().Alert("Woooooooooooooooooooooww! Alert2!")
+	return nil
+}
